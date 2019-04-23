3f860c8df4cff4559dabae343ee8337def79f89e
Won Kim <wonsae.kim@gmail.com>
2019-04-23 21:39:00 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    @@artists << artist unless @@artists.include?(artist)
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+    #binding.pry
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.genre_count
+    @@genres.each do |e|
+      if @@genre_count[e] == nil
+        @@genre_count[e] = 1
+      else
+        @@genre_count[e] += 1
+      end
+    end
+  end
+  
+  def self.artist_count
+    @@artists.each do |e|
+      if @@artist_count[e] == nil
+        @@artist_count[e] = 1
+      else
+        @@artist_count[e] += 1
+      end
+    end
+  end
+end
